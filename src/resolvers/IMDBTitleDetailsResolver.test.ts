679e0befd1ee6bc14c0fdfb30f1e8b505dd208fe
Zoha Banam <zoha.banam@gmail.com>
2022-04-14 04:56:51 +0430

    update rate for test cases

--- src/resolvers/IMDBTitleDetailsResolver.test.ts
+++ src/resolvers/IMDBTitleDetailsResolver.test.ts
@@ -122,7 +122,7 @@ const titlesToTest: ITitleTestData[] = [
       ],
     },
     mainRate: {
-      rate: 7.9,
+      rate: 7.8,
       minVotesCount: 1100000,
       assortedByRateLength: 10,
     },
