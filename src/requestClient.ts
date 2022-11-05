ec55ae2a2b733b1d2c67aee71eee29cf8bf196bd
Zoha <Zoha.Banam@gmail.com>
2022-11-06 01:07:21 +0330

    set cookie on requests

--- src/requestClient.ts
+++ src/requestClient.ts
@@ -11,6 +11,8 @@ export function getRequest(
   return axios.get(url, {
     headers: {
       "accept-language": "en-US,en",
+      Cookie:
+        "session-id=143-9737041-2029012; session-id-time=2298403685; session-token=j4EejTFisPL9vXbxJKIimGbuAk+eqCEgUi7Bua5GYy9u0Zid6InLR6c6L4fw+8/JBJlxpZPgfHDJJnzugb3DAs7m6x7DnUca7gP3dGgidMx8LdhF8GK4wlEaDe6SbitGlLv8Gk8hFjdM9BOFQ+auwq5nNfk6yaA2aK3mugUiR2Lc0mTm3hwzCZbLAgxjd5zBXc+B4h3CG0+lZJ3l1jLdjcXsPiCmRD6+Up48G9bLtB4=; ubid-main=130-5229443-8579023; uu=eyJpZCI6InV1YTBjZTRkZTMyZWExNDQ1N2IyMzYiLCJwcmVmZXJlbmNlcyI6eyJmaW5kX2luY2x1ZGVfYWR1bHQiOmZhbHNlfX0=",
     },
     params,
   });
